a7e96acf3c227243b54f7bd012dd449c59220b9a
ravivco <raviv@go-public.co.il>
2018-05-21 19:49:35 +0300

    Query meta additionsd

--- src/Traits/BuilderTrait.php
+++ src/Traits/BuilderTrait.php
@@ -20,6 +20,8 @@ trait BuilderTrait
 
     protected $body;
 
+    protected $meta;
+
     protected $argKeys = [];
 
     protected $bodyPrototype = [];
@@ -57,10 +59,17 @@ trait BuilderTrait
         if (empty($this->arguments)) {
             $this->processArgumentsNames($arguments);
 
-            $this->arguments = $this->replacePlaceholders(sprintf('(%s)', substr(json_encode($arguments), 1, strlen(json_encode($arguments)) - 2)));
+            $this->arguments = $this->replacePlaceholders(
+                sprintf(
+                    '(%s)',
+                    substr(json_encode($arguments, JSON_UNESCAPED_SLASHES),
+                    1,
+                    strlen(json_encode($arguments, JSON_UNESCAPED_SLASHES)) - 2))
+            );
         }
 
         $this->processEnums();
+        $this->processOrderBy();
 
         return $this;
     }
@@ -77,7 +86,20 @@ trait BuilderTrait
                 $this->arguments = str_replace(sprintf('"%s"', $value), $value, $this->arguments);
             }
         }
+
         $this->arguments = str_replace('ENUM_', '', $this->arguments);
+    }
+
+
+    protected function processOrderBy()
+    {
+        foreach ($this->values as $value) {
+            if(strpos($value, 'ORDER_') !== false) {
+                $this->arguments = str_replace(sprintf('"%s"', $value), $value, $this->arguments);
+            }
+        }
+
+        $this->arguments = str_replace('ORDER_', '', $this->arguments);
     }
 
     /**
@@ -101,6 +123,71 @@ trait BuilderTrait
     public function resetBody()
     {
         $this->body = null;
+    }
+
+    /**
+     * @return BuilderInterface|BuilderTrait $this
+     *
+     */
+    public function meta()
+    {
+        $this->meta = $this->getMetaName() . $this->getMetaArguments() . $this->getMetaBody();
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    private function getMetaName()
+    {
+        return $this->name.'Connection';
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getMetaArguments()
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return string
+     */
+    private function getMetaBody()
+    {
+        return Util::PARSER_L_BRACE . $this->processMetaCount() . $this->processMetaPageInfo() . Util::PARSER_R_BRACE;
+    }
+
+    private function processMetaCount()
+    {
+        $meta_body_str = 'aggregate' . Util::PARSER_L_BRACE;
+        $meta_body_str .= 'count' . Util::PARSER_R_BRACE;
+        $meta_body_str .= Util::PARSER_EOL;
+
+        return $meta_body_str;
+    }
+
+
+    private function processMetaPageInfo()
+    {
+        $meta_body_str = 'pageInfo' . Util::PARSER_L_BRACE;
+        $meta_body_str .= 'hasNextPage' . Util::PARSER_EOL;
+        $meta_body_str .= 'hasPreviousPage' . Util::PARSER_EOL;
+        $meta_body_str .= 'startCursor' . Util::PARSER_EOL;
+        $meta_body_str .= 'endCursor' . Util::PARSER_EOL;
+        $meta_body_str .= Util::PARSER_R_BRACE;
+
+        return $meta_body_str;
+    }
+
+    /**
+     *
+     */
+    public function resetMeta()
+    {
+        $this->meta = null;
     }
 
     protected function processArgumentsNames(array $arguments)
